179e5ddef3a810ef8d8d487fab3630a7ab57e304
ascha10 <ascha9957@gmail.com>
2022-04-06 02:15:54 +0300

    function added to server

--- server/models/buisinessModel.js
+++ server/models/buisinessModel.js
@@ -1,16 +1,12 @@
 const mongoose = require("mongoose");
 const  Business = new mongoose.Schema(
   {
-    firstName: { type: String, require: true },
-    lastName: { type: String, require: true },
-    email: { type: String, require: true, unique: true },
     image:{type: String ,default:"" },
     phoneNumber:{type: String, require: true},
     isAdmin:{type: Boolean},
     businessName:{type: String, require: true},
     businessNumber:{type: String, require: true},
-    password:{type: String, require: true}
-
+    
   },
 
   { timestamps: true }
