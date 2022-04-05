6aa56a1ce858a24a90d28fd3bf3ee5daa3027b81
avi-admaso <aviadmaso@gmail.com>
2022-04-05 17:36:37 +0300

    create login and register

--- server/routes/buisinessRoute.js
+++ server/routes/buisinessRoute.js
@@ -1,11 +1,13 @@
 const buisinessRoutes = require("express").Router();
 
-const { GetBuisiness,GetBuisinessById, AddBuisiness, UpdateBuisiness, DeleteBuisiness } = require("../controllers/buisinessController");
+const { GetBuisiness,GetBuisinessById, AddBuisiness, UpdateBuisiness, DeleteBuisiness,register,login } = require("../controllers/buisinessController");
 
 buisinessRoutes.get("/", GetBuisiness);
 buisinessRoutes.get("/:id", GetBuisinessById);
 buisinessRoutes.post("/", AddBuisiness);
 buisinessRoutes.put("/:id", UpdateBuisiness);
 buisinessRoutes.delete("/:id", DeleteBuisiness);
+userRoutes.post("/register", register);
+userRoutes.post("/login", login);
 
 module.exports = buisinessRoutes;
--- server/routes/userRoute.js
+++ server/routes/userRoute.js
@@ -1,11 +1,14 @@
 const userRoutes = require("express").Router();
 
-const { GetUserById, Updateuser, Deleteuser, Adduser, GetUsers } = require("../controllers/userController");
+const { GetUserById, Updateuser, Deleteuser, Adduser, GetUsers,login,register } = require("../controllers/userController");
 
 userRoutes.get("/", GetUsers);
 userRoutes.get("/:id", GetUserById);
 userRoutes.post("/", Adduser);
 userRoutes.put("/:id", Updateuser);
 userRoutes.delete("/:id", Deleteuser);
+userRoutes.post("/register", register);
+userRoutes.post("/login", login);
+
 
 module.exports = userRoutes;
