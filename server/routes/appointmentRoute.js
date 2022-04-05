ef034df9ed988a40847ff34c004f49d660bbf1b9
avi-admaso <aviadmaso@gmail.com>
2022-04-05 17:54:41 +0300

    add slash

--- server/routes/appointmentRoute.js
+++ server/routes/appointmentRoute.js
@@ -6,6 +6,6 @@ AppointmentsRoutes.get("/appointment", GetAppointments);
 AppointmentsRoutes.get("/appointment/:id", GetAppointmentById);
 AppointmentsRoutes.post("/appointment", AddAppointment);
 AppointmentsRoutes.put("/appointment/:id", UpdateAppointment);
-AppointmentsRoutes.delete("/appointment:id", DeleteAppointment);
+AppointmentsRoutes.delete("/appointment/:id", DeleteAppointment);
 
 module.exports = AppointmentsRoutes;
