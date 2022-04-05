33a26daea8645bb0813b17116588c001dd84e96b
ascha10 <ascha9957@gmail.com>
2022-04-05 19:47:39 +0300

    grid completed

--- client/src/Router/RouterApp.jsx
+++ client/src/Router/RouterApp.jsx
@@ -7,28 +7,31 @@ import Page404 from "../Components/Pages/Page404/Page404";
 import Profile from "../Components/Pages/Profile/Profile";
 import RequiredAuth from "../Components/parts/RequiredAuth/RequiredAuth"
 import ContactUs from "../Components/Pages/ContactUs/ContactUs";
-import Calendar from "../Components/Pages/Calendar/Calendar";
 import HistoricalAppointment from "../Components/Pages/HistoricalAppointment/HistoricalAppointment";
 import NextAppointment from "../Components/Pages/NextAppointment/NextAppointment";
 import SetAppointment from "../Components/Pages/SetAppointment/SetAppointment";
 import SideBar from "../Components/parts/SideBar/SideBar";
+import Calendars from "../Components/Pages/Calendar/Calendars";
+import NavBar from "../Components/parts/navbar/Nav-Bar"
 
 
 export default function RouterApp(){
   return (
     <BrowserRouter>
+        <NavBar/>
+        <SideBar/>
       <Routes>
         {/** Public Routes **/}
         <Route path="/" element={<Home />} />
         <Route path="/SignUp" element={<SignUp />} />
         <Route path="/Login" element={<Login />} />
         <Route path="/ContactUs" element={<ContactUs />} />
+        
 
         {/** Private Routes **/}
         {/* <Route element={<RequiredAuth />}> */}
-             <Route path="/SideBar" element={<SideBar />} />
              <Route path="/Profile" element={<Profile />} />
-             <Route path="/Calender" element={<Calendar/>}/>
+             <Route path="/Calender" element={<Calendars/>}/>
              <Route path="/HistoricalAppointment" element={<HistoricalAppointment/>}/>
              <Route path="/SetAppointment" element={<SetAppointment/>}/>
              <Route path="/NextAppointment" element={<NextAppointment/>}/>
