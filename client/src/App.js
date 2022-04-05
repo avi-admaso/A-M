8e81194fea39ef91521f8479a9c7187737a50609
avi-admaso <aviadmaso@gmail.com>
2022-04-05 23:55:54 +0300

    change footers

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,12 @@
 import "./App.css";
 import RouterApp from "./Router/RouterApp";
+import footer from "./Router/Footers";
 
 function App() {
   return (
     <div className="App">
      <RouterApp/>
-     
+     <footer/>
     </div>
   );
 }
