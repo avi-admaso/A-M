9b22885b6e5688633889dfab19869195cd623c4c
ascha10 <ascha9957@gmail.com>
2022-04-06 04:18:50 +0300

    appointemnet

--- server/controllers/appointmentController.js
+++ server/controllers/appointmentController.js
@@ -23,9 +23,11 @@ module.exports = {
     },
     GetAppointmentOfBusiness: async (req, res) => {
         try {
-            const appointment = await appointment.find({ businessName: req.params.BusinessName });
-            if (appointment) return res.status(200).json({ success: true, appointment });
-            res.status(404).json({ success: false, message: "no appointments found" });
+            await appointment.find({ businessName: req.params.BusinessName }).then((appointment) => {
+
+                if (appointment) return res.status(200).json({ success: true, appointment });
+                res.status(404).json({ success: false, message: "no appointments found" });
+            })
         }
         catch (err) {
             res.status(500).json({ success: false, message: err.message });
