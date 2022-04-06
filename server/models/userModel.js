e53f1be89bba55c62fb949c41691841500ee3cdf
ascha10 <ascha9957@gmail.com>
2022-04-06 03:55:26 +0300

    scehma updated

--- server/models/userModel.js
+++ server/models/userModel.js
@@ -1,16 +1,16 @@
 const mongoose = require("mongoose");
 const User = new mongoose.Schema(
   {
-    firstName: { type: String, required },
-    lastName: { type: String, required },
-    email: { type: String, required, unique: true },
-    image:{type: String , default:""},
-    phoneNumber:{type: String, required},
+    firstName: { type: String, required: true },
+    lastName: { type: String, required: true },
+    email: { type: String, required: true, unique: true },
+    image:{type: String , default:"https://pixabay.com/vectors/blank-profile-picture-mystery-man-973460/"},
+    phoneNumber:{type: String, required: true},
     isAdmin:{type: Boolean},
     historyAppointment:{type: Array},
     nextAppointment:{type: Array},
     businessName:{type: String, default:""},
-    password:{type: String, required}
+    password:{type: String, required: true}
   },
   { timestamps: true }
 );
