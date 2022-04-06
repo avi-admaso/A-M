d6dd128d3d19bad6d6dea63fc9471c4ec35a406b
YafitSamuel <gyafit642@gmail.com>
2022-04-06 10:47:34 +0300

    .

--- client/src/Components/parts/EditProfile/EditProfile.jsx
+++ client/src/Components/parts/EditProfile/EditProfile.jsx
@@ -2,10 +2,12 @@
 import React, { useContext ,useState} from "react";
 import { UserContext } from "../../../context/UserContext";
 import {UpdateUser} from "../../../services/userService"
+import   "./EditProfile.css"
 
 const EditProfile = () => {
     const { user , setU} = useContext(UserContext);
     const [upDate,setUpData]=useState({})
+
 const InputValue =(e)=>{
     upDate[e.target.name]=e.target.value
     setUpData({...upDate})
@@ -15,16 +17,22 @@ const InputValue =(e)=>{
         console.log(user._id);
         UpdateUser(user._id,upDate).then((res) => console.log(res));
     }
+
+
   return (
-    <div>
-      <form>
+    <div className="EditProfile">
+       
+              <button className='close_editPopUp' onClick={nn}>x</button>
+ <form>
+          <h1 >Edit Profile</h1><br/>
         <input type="text" name="firstName" onChange={InputValue} defaultValue={user.firstName} />
         <input type="text" name="lastName" deonChange={InputValue} defaultValue={user.lastName} />
         <input type="text" name="email" onChange={InputValue} defaultValue={user.email}  />
         <input type="text" name="image" onChange={InputValue} defaultValue={user.image} />
-        <input type="text" name="phoneNumber" onChange={InputValue} defaultValue={user.phoneNumber } />
-        <button onClick={tt}>save</button>
+        <input type="text" name="phoneNumber" onChange={InputValue} defaultValue={user.phoneNumber } /><br/>
+       <button onClick={tt} >save</button>
       </form>
+      
     </div>
   );
 };
