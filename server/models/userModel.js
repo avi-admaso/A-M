fdb98cd614e7a0da5ed94378dce84332cc67eb58
ascha10 <ascha9957@gmail.com>
2022-04-06 03:39:21 +0300

    ss

--- server/models/userModel.js
+++ server/models/userModel.js
@@ -1,16 +1,16 @@
 const mongoose = require("mongoose");
 const User = new mongoose.Schema(
   {
-    firstName: { type: String, require: true },
-    lastName: { type: String, require: true },
-    email: { type: String, require: true, unique: true },
+    firstName: { type: String, required },
+    lastName: { type: String, required },
+    email: { type: String, required, unique: true },
     image:{type: String , default:""},
-    phoneNumber:{type: String, require: true},
+    phoneNumber:{type: String, required},
     isAdmin:{type: Boolean},
     historyAppointment:{type: Array},
     nextAppointment:{type: Array},
-    businessName:{type: String, require: true},
-    password:{type: String, require: true}
+    businessName:{type: String, default:""},
+    password:{type: String, required}
   },
   { timestamps: true }
 );
